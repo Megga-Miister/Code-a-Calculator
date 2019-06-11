ba6bdb4dfe546032c9a299e95e187294f428becd
Meggan <meggtriplett@gmail.com>
2019-06-11 01:43:08 -0700

    Basic scaffolding and completed Add method.

---
+++ Code A Calculator/Code A Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Code_A_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose what type of calculation you would like: Add, Subtract, Multiply or Divide");
+            string calculate = Console.ReadLine();
+
+
+            //Console.WriteLine("Please enter what number you would like to " + calculate + ".");
+            //string stringA = Console.ReadLine();
+            //int numberA = Convert.ToInt32(stringA);
+
+            //Console.WriteLine("Please enter the other number you would like to " + calculate + " with.");
+            //string stringB = Console.ReadLine();
+            //int numberB = Convert.ToInt32(stringB);
+        }
+
+        static int Add(int givenNumberA, int givenNumberB)
+        {
+            int sum = givenNumberA + givenNumberB;
+            return sum;
+        }
+
+        //static int Subtract(int givenNumberA, int givenNumberB)
+        //{
+
+        //}
+
+        //static int Multiply(int givenNumberA, int givenNumberB)
+        //{
+
+        //}
+
+        //static int Divide(int givenNumberA, int givenNumberB)
+        //{
+
+        //}
+    }
+}
